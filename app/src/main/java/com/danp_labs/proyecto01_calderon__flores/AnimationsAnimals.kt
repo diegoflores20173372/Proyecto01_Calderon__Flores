6a203cab1d742e464b66ac0dde8bc56bbed38942
diegoflores20173372 <dflorescam@unsa.edu.pe>
2022-05-16 19:02:58 -0500

    Initial commit

---
+++ app/src/main/java/com/danp_labs/proyecto01_calderon__flores/AnimationsAnimals.kt
@@ -0,0 +1,58 @@
+package com.danp_labs.proyecto01_calderon__flores
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.danp_labs.proyecto01_calderon__flores.ui.theme.Proyecto01_Calderon__FloresTheme
+
+class AnimationsAnimals : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Proyecto01_Calderon__FloresTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    AnimationAnimalsContainer("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AnimationAnimalsContainer(name: String) {
+    val context = LocalContext.current
+
+
+    Column {
+        Button(onClick = {
+            val intent = Intent(context, DetailAnimalExtinction::class.java)
+            context.startActivity(intent)
+        }) {
+            Text("Regresar a Detalles de Animales")
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    Proyecto01_Calderon__FloresTheme {
+        AnimationAnimalsContainer("Android")
+    }
+}
