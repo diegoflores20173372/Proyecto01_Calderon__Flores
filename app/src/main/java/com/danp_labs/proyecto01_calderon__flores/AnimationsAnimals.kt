4b17a2c18bbc4f1c462fdb3530c371305c673a29
ValeriaCalderon123 <valerianicoll.calderonr@gmail.com>
2022-05-19 04:05:08 -0500

    Changes on cards

--- app/src/main/java/com/danp_labs/proyecto01_calderon__flores/AnimationsAnimals.kt
+++ app/src/main/java/com/danp_labs/proyecto01_calderon__flores/AnimationsAnimals.kt
@@ -4,16 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.danp_labs.proyecto01_calderon__flores.ui.theme.Proyecto01_Calderon__FloresTheme
 
 class AnimationsAnimals : ComponentActivity() {
@@ -26,6 +30,7 @@ class AnimationsAnimals : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
                     AnimationAnimalsContainer("Android")
                 }
             }
@@ -45,6 +50,18 @@ fun AnimationAnimalsContainer(name: String) {
         }) {
             Text("Regresar a Detalles de Animales")
         }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            SwipeableCard(
+                dataSource = (0..10).map { 0 }.toList()
+            )
+
+        }
+
     }
 
 }
